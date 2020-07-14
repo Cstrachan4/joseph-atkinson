dc5df5b74f27009f822917799bfb007320c7cd7d
Callum Strachan <callum.strachan@appliedworks.co.uk>
2020-07-14 13:31:57 +0100

    Add pdf viewer and portfolio page

---
+++ pages/portfolio.js
@@ -0,0 +1,18 @@
+import HeadMeta from '../components/HeadMeta';
+import PdfViewer from '../components/PdfViewer';
+
+import '../lib/styles.js';
+
+function Portfolio() {
+
+  return (
+  	<React.Fragment>
+	  	<HeadMeta title="Joseph Atkinson" description="Joseph Atkinson is a designer and digital art director who lives and works in London. Currently designing interfaces with TBC.work" />
+	  	<div className="portfolio">
+        <PdfViewer fileUrl="./files/JA_Portfolio_mid2020.pdf" />
+	  	</div>
+  	</React.Fragment>
+  )
+}
+
+export default Portfolio;
