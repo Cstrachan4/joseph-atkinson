6ecbe670c10094cb2f6ca5366f062f3d7ad3c699
Callum Strachan <callum.strachan@appliedworks.co.uk>
2021-02-23 20:19:22 +0000

    Update to latest next

--- pages/_document.js
+++ pages/_document.js
@@ -1,39 +1,17 @@
-import Document, { Head, Main, NextScript } from "next/document" 
+import Document, {Html, Head, Main, NextScript } from "next/document"
 import { readFileSync } from "fs"
 import { join } from "path"
-
-class InlineStylesHead extends Head {
-  getCssLinks() {
-    return this.__getInlineStyles()
-  }
-
-  __getInlineStyles() {
-    const { assetPrefix, files } = this.context._documentProps
-    if (!files || files.length === 0) return null
-
-    return files.filter(file => /\.css$/.test(file)).map(file => (
-      <style
-        key={file}
-        nonce={this.props.nonce}
-        data-href={`${assetPrefix}/_next/${file}`}
-        dangerouslySetInnerHTML={{
-          __html: readFileSync(join(process.cwd(), ".next", file), "utf-8")
-        }}
-      />
-    ))
-  }
-}
 
 export default class CustomDocument extends Document {
   render() {
     return (
-      <html>
-        <InlineStylesHead />
+      <Html lang="en">
+        <Head />
         <body>
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     )
   }
 }
